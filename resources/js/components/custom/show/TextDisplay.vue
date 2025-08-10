4ce602c48215a9c3b5bdf45ba7fa890736ca9f0b
Leo <leo@innovit.co.za>
2025-08-10 14:49:21 +0200

    Minor style tweak on the show page

--- resources/js/components/custom/show/TextDisplay.vue
+++ resources/js/components/custom/show/TextDisplay.vue
@@ -6,12 +6,12 @@ const props = defineProps<{
 </script>
 
 <template>
-    <div class="mt-8 border border-dashed p-2">
+    <div class="mt-8 rounded border border-dashed p-4">
         <div class="flex justify-between">
             <h3 class="text-2xl font-bold">{{ props.title }}</h3>
 
             <div
-                class="rounded px-2 py-1 text-sm"
+                class="flex items-center rounded px-2 py-1 text-sm"
                 :class="{
                     'border-green-500 bg-green-500/20 text-green-500': props.status == 'complete',
                     'border-orage-500 bg-orange-500/20 text-orange-500': props.status == 'progress',
