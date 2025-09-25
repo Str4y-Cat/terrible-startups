97871db55bf591e8bfb0a53a81755b2a14f42f67
Leo <leo@innovit.co.za>
2025-09-25 13:23:11 +0200

    Refactored install prompt

--- resources/js/components/custom/sidebar/InstallPrompt.vue
+++ resources/js/components/custom/sidebar/InstallPrompt.vue
@@ -1,37 +1,10 @@
 <script setup lang="ts">
 import Button from '@/components/ui/button/Button.vue';
-// import { ref } from 'vue';
 
-// const canInstall = ref(false);
-// let installPrompt = null;
-//
-// window.addEventListener('beforeinstallprompt', (event) => {
-//     console.log('beforeinstallprompt');
-//     event.preventDefault();
-//     installPrompt = event;
-//     canInstall.value = true;
-// });
-//
-// window.addEventListener('appinstalled', () => {
-//     disableInAppInstallPrompt();
-// });
-//
-// async function tryInstall() {
-//     if (!canInstall.value) {
-//         console.log('cannot install');
-//         return;
-//     }
-//
-//     const result = await installPrompt.prompt();
-//     console.log(`Install prompt was: ${result.outcome}`);
-//     disableInAppInstallPrompt();
-// }
-//
-// function disableInAppInstallPrompt() {
-//     installPrompt = null;
-//     canInstall.value = false;
-// }
+import { useInstallPWA } from '@/composables/usePWAInstall';
+const { canInstall, isInstalled, install } = useInstallPWA();
+// import { ref } from 'vue';
 </script>
 <template>
-    <Button @click.prevent="console.log('boo')" :disabled="false"> Install </Button>
+    <Button @click.prevent="install" :disabled="!canInstall" v-if="!isInstalled"> Install </Button>
 </template>
--- resources/js/composables/usePWAInstall.ts
+++ resources/js/composables/usePWAInstall.ts
@@ -1,39 +1,102 @@
-// composables/usePWAInstall.ts
+// // composables/usePWAInstall.ts
+//
+// import { ref } from 'vue';
+// const canInstall = ref(false);
+// const installPrompt = ref<Event | null>(null);
+//
+// async function tryInstall() {
+//     if (!canInstall.value) {
+//         console.log('cannot install');
+//         return;
+//     }
+//
+//     const result = await installPrompt.value.prompt();
+//     console.log(`Install prompt was: ${result.outcome}`);
+//     disableInAppInstallPrompt();
+// }
+//
+// function disableInAppInstallPrompt() {
+//     installPrompt.value = null;
+//     canInstall.value = false;
+// }
+//
+// export function usePWAInstall() {
+//     window.addEventListener('beforeinstallprompt', (event) => {
+//         console.log('beforeinstallprompt', event);
+//         event.preventDefault();
+//         installPrompt.value = event;
+//         canInstall.value = true;
+//     });
+//
+//     window.addEventListener('appinstalled', () => {
+//         disableInAppInstallPrompt();
+//     });
+//
+//     return {
+//         canInstallPWA: canInstall,
+//         installPWA: tryInstall,
+//     };
+// }
+//
+import { readonly, ref } from 'vue';
 
-import { ref } from 'vue';
 const canInstall = ref(false);
+const isInstalled = ref(false);
 const installPrompt = ref<Event | null>(null);
 
-async function tryInstall() {
-    if (!canInstall.value) {
-        console.log('cannot install');
-        return;
-    }
+//singleton flag
+let isInitialized = false;
 
-    const result = await installPrompt.value.prompt();
-    console.log(`Install prompt was: ${result.outcome}`);
-    disableInAppInstallPrompt();
-}
-
-function disableInAppInstallPrompt() {
+function reset() {
     installPrompt.value = null;
     canInstall.value = false;
 }
 
-export function usePWAInstall() {
+function initializePWA() {
+    if (isInitialized) return;
+    isInitialized = true;
+
+    //listen for the install prompt event
     window.addEventListener('beforeinstallprompt', (event) => {
-        console.log('beforeinstallprompt');
         event.preventDefault();
         installPrompt.value = event;
         canInstall.value = true;
+
+        console.log('PWA can be installed!', event);
     });
 
+    //listen for a successfull installation
     window.addEventListener('appinstalled', () => {
-        disableInAppInstallPrompt();
+        console.log('pwa is installed!');
+        reset();
+        isInstalled.value = true;
     });
+
+    if (window.matchMedia('(display-mode: standalone)').matches) {
+        isInstalled.value = true;
+    }
+}
+
+async function installPWA() {
+    if (!canInstall.value || !installPrompt.value) {
+        console.log('PWA cannot be installed currently');
+        return;
+    }
+
+    const promptEvent = installPrompt.value;
+    const result = await promptEvent.prompt();
+    console.log(`Result of installation is: ${result}`);
+
+    reset();
+}
+
+export function useInstallPWA() {
+    //initialize pwa singleton
+    initializePWA();
 
     return {
-        canInstallPWA: canInstall,
-        installPWA: tryInstall,
+        canInstall: readonly(canInstall),
+        isInstalled: readonly(isInstalled),
+        install: installPWA,
     };
 }
