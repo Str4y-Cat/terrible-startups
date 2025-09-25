0dfeff40ba2641cf5844cb40a5fe9a1e2fba58db
Leo <leo@innovit.co.za>
2025-09-25 15:14:05 +0200

    Fixed install prompt for ios, firefox and others

--- resources/js/components/custom/sidebar/InstallPrompt.vue
+++ resources/js/components/custom/sidebar/InstallPrompt.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import Button from '@/components/ui/button/Button.vue';
+
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { CardContent } from '@/components/ui/card';
+import { Dialog, DialogClose, DialogContent, DialogHeader, DialogTrigger } from '@/components/ui/dialog';
 import {
     Drawer,
     DrawerClose,
@@ -13,37 +16,78 @@ import {
 } from '@/components/ui/drawer';
 import { useInstallPWA } from '@/composables/usePWAInstall';
 import { createReusableTemplate, useMediaQuery } from '@vueuse/core';
-import { Download, Share, Smartphone } from 'lucide-vue-next';
+import { Download, MonitorDown, Share, Smartphone } from 'lucide-vue-next';
+import { computed } from 'vue';
 
 const { canInstall, isInstalled, install } = useInstallPWA();
 // import { ref } from 'vue';
 
-function isIOS(): boolean {
+const isIOS = computed(() => {
     return ['iPad Simulator', 'iPhone Simulator', 'iPod Simulator', 'iPad', 'iPhone', 'iPod'].indexOf(navigator.platform) !== -1;
-}
+});
 
-function isFirefox(): boolean {
+const isFirefox = computed(() => {
     return navigator.userAgent.indexOf('Firefox') > -1;
-}
+});
 
 const [UseTemplate, InstallGuide] = createReusableTemplate();
 const isDesktop = useMediaQuery('(min-width: 768px)');
 </script>
 <template>
-    <Button @click.prevent="install" :disabled="!canInstall" v-if="!isInstalled && !isIOS() && !isFirefox()"> Install </Button>
-    <h1 class="flex animate-pulse justify-center rounded bg-blue-500 py-2 text-black" v-if="isIOS()">THIS IS IOS</h1>
-    <h1 class="flex animate-pulse justify-center rounded bg-orange-500 py-2 font-bold text-black" v-if="isFirefox()">THIS IS FIREFOX</h1>
+    <Button @click.prevent="install" :disabled="!canInstall" v-if="!isInstalled && !isIOS && !isFirefox">
+        <Download class="size-5" /> Install app
+    </Button>
 
     <UseTemplate>
-        <CardContent class="bt-8 p-4 pb-16">
-            <ol class="ml-5 list-decimal text-xl font-bold">
+        <CardContent class="p-4">
+            <ol v-if="!isDesktop" class="ml-5 list-decimal text-xl font-bold">
                 <li><span>Click the </span><Share class="mx-2 inline size-5" /> <span> icon on the browser bar</span></li>
                 <li class="mt-3">Select "Add to Home Screen".</li>
+            </ol>
+
+            <ol v-if="isDesktop && isFirefox" class="ml-5 list-decimal text-xl font-bold">
+                <li>Look at the browser search bar, at the top of your screen</li>
+                <li class="mt-3">Select the download icon <MonitorDown class="mx-2 inline size-5" /></li>
             </ol>
         </CardContent>
     </UseTemplate>
 
-    <Drawer v-if="!canInstall">
+    <Dialog v-if="isDesktop && !canInstall">
+        <DialogTrigger>
+            <Button :disabled="canInstall" class="w-full"> <Download class="size-5" /> Install app </Button>
+        </DialogTrigger>
+
+        <DialogContent class="">
+            <DialogHeader class="flex-row items-center gap-4">
+                <div class="w-fit rounded-full bg-primary p-4">
+                    <Smartphone />
+                </div>
+                <div>
+                    <DrawerTitle class="">Install method</DrawerTitle>
+                    <DrawerDescription>Follow the steps below to install this web app on your device</DrawerDescription>
+                </div>
+            </DialogHeader>
+
+            <Alert v-if="isFirefox">
+                <AlertTitle>Heads up!</AlertTitle>
+                <AlertDescription>
+                    Note: As you are using firefox, the steps may vary. If you are not able to follow the steps provided, please consider a different
+                    browser for the install
+                </AlertDescription>
+            </Alert>
+
+            <!-- Rating form -->
+            <InstallGuide class="by-8" />
+
+            <DialogFooter>
+                <DialogClose as-child>
+                    <Button type="button" variant="ghost" class="text-muted-foreground"> Keep using app in broswer </Button>
+                </DialogClose>
+            </DialogFooter>
+        </DialogContent>
+    </Dialog>
+
+    <Drawer v-if="!canInstall && !isDesktop">
         <DrawerTrigger>
             <Button :disabled="canInstall" class="w-full"> <Download class="size-5" /> Install app </Button>
         </DrawerTrigger>
@@ -60,7 +104,7 @@ const isDesktop = useMediaQuery('(min-width: 768px)');
                     </div>
                 </DrawerHeader>
 
-                <InstallGuide></InstallGuide>
+                <InstallGuide class="bt-8 pb-16"></InstallGuide>
 
                 <DrawerFooter class="pt-2">
                     <DrawerClose as-child>
